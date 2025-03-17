3983597f3966134c065a68626189413f0be60f6f
dsareen <dk930654@dal.ca>
2025-03-16 22:12:38 -0400

    First jwt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Home
+      <br />
+      <ol>
+        <li>
+          <a href={"/jwt-home"}>JWT Home</a>
+        </li>
+        <li>
+          <a href={"/jwt-safehouse"}>JWT Safe house</a>
+        </li>
+      </ol>
     </div>
   );
 }
